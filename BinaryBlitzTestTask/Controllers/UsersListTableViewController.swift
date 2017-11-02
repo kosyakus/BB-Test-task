4ef609791bcb3ed440641a945a13ffed916727b6
Unknown <kosyak_@mail.ru>
2017-11-02 16:29:08 +0300

    reloading data after closing modal view

--- BinaryBlitzTestTask/Controllers/UsersListTableViewController.swift
+++ BinaryBlitzTestTask/Controllers/UsersListTableViewController.swift
@@ -15,7 +15,11 @@ class UsersListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+ 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         downloadUsers(completion: { (success) in
             if success {
                 print("success")
@@ -23,7 +27,6 @@ class UsersListTableViewController: UITableViewController {
                 self.tableView.reloadData()
             }
         })
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,8 +48,6 @@ class UsersListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UsersListCell", for: indexPath) as! UsersTableViewCell
         
-        //let yourObject = dataArray[[dataArray count] - 1 - indexPath.row];
-        //cell.textLabel.text = yourObject
         let user = users.reversed()[indexPath.row]
         
         cell.nameLabel.text = "\(user.name) \(user.surname)"
@@ -67,40 +68,13 @@ class UsersListTableViewController: UITableViewController {
         
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    
 
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+    
 
     
     // MARK: - Navigation
