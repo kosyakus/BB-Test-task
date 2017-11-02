48838141e30b98fbe3c6c9f534362ff6080580f6
Unknown <kosyak_@mail.ru>
2017-11-02 17:08:43 +0300

    Decorated the cell

--- BinaryBlitzTestTask/Controllers/UsersTableViewCell.swift
+++ BinaryBlitzTestTask/Controllers/UsersTableViewCell.swift
@@ -18,8 +18,11 @@ class UsersTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        self.thumbnail.layer.cornerRadius = self.thumbnail.frame.size.width / 2;
+        //self.thumbnail.layer.cornerRadius = self.thumbnail.frame.size.width / 2;
         self.thumbnail.clipsToBounds = true
+        self.thumbnail.layer.cornerRadius = 20.0
+        self.thumbnail.layer.borderWidth = 3.0
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
